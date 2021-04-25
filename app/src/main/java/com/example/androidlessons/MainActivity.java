2580e4c0320753c35dbb712f4835534b0659ff27
Ekaterina <yeldog@mail.ru>
2021-04-25 16:56:47 +0300

    Lesson 1: Buttoms added

--- app/src/main/java/com/example/androidlessons/MainActivity.java
+++ app/src/main/java/com/example/androidlessons/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.androidlessons;
-
+//активити - это экран
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Привет!", Toast.LENGTH_LONG).show();
+            }
+        });
     }//commit///
+    //TODO
+    //FIXME
 }
