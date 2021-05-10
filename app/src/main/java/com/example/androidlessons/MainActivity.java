ec0f367d08544bf44efd509823fdb8817e57fe01
Ekaterina <yeldog@mail.ru>
2021-05-10 23:49:59 +0300

    start

--- app/src/main/java/com/example/androidlessons/MainActivity.java
+++ app/src/main/java/com/example/androidlessons/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.androidlessons;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
     }//commit///
+
+    private void initView() {
+        Button buttonGreeting = findViewById(R.id.buttonGreating);
+        final TextView textView = findViewById(R.id.textHello);
+        final EditText editText = findViewById(R.id.textName);
+        buttonGreeting.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                String name = editText.getText().toString();
+                //это чтобы эдатабл, который возврщает гет текст, превратить в стринг
+                String sayText = " Привет, " + name;
+                textView.setText(sayText);
+            }
+        });
+    }
 }
