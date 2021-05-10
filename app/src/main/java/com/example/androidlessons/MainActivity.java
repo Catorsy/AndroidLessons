7b3aef641f1e79127d33395457b25f75382dfb97
Ekaterina <yeldog@mail.ru>
2021-05-10 23:51:50 +0300

    start

--- app/src/main/java/com/example/androidlessons/MainActivity.java
+++ app/src/main/java/com/example/androidlessons/MainActivity.java
@@ -8,6 +8,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+//Я заглушка, не надо меня пока смотреть. Планирую превратиться в ДЗ до четверга включительно.
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private TextView textView;
     private Calculations calculations;
